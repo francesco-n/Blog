6b2d69552580709f70428e4f4f2d31fd7e3bc63b
fratornix <francesco.negriolli@protonmail.com>
2020-08-24 15:21:19 +0200

    Commentato e riordinato newclog.sh

--- newclog.sh
+++ newclog.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
 
-DT=$(date)
-INDEX_FILE=index
-HTMLFILE="articoli/indipendenzadigitale.html"
-
 FILE_COUNT=$(( $(ls clog | grep -e '[0-9+]' | wc -l) + 1 )) 
 
-echo -n "Titolo del post: "
-read TITLE
-read -p "File da copiare (n se vuoi crearne uno nuovo): " HTMLFILE
+read -p "Post title: " TITLE
+read -p "File to copy: (type n if you want to create a new post) " HTMLFILE
 if [ $HTMLFILE != "n" ]
 then
 	HTMLFILE="articoli/$HTMLFILE"
 	if [ ! -f $HTMLFILE ]
 	then
-		echo "Il file non esiste!" >&2
+		echo "File does not exist" >&2
 		exit
 	else
-		#printf "\e[2m%s\e[0m\n" $DT >> clog/$FILE_COUNT
-		#printf "\e[32m%s\e[0m\n\n" $TITLE >> clog/$FILE_COUNT
-		TESTO=$(grep -o '<[p|h][1-9]*[^>]*>.*</[p|h][1-9]*>' $HTMLFILE | \
+		TEXT=$(grep -o '<[p|h][1-9]*[^>]*>.*</[p|h][1-9]*>' $HTMLFILE | \
 			sed -e 's/<h[1-9][^>]*>/\o033[32m/' \
 	    		-e 's/<\/h[1-9]>/\o033[0m/' \
 	    		-e 's/<strong>/\o033[4m/' \
 	    		-e 's/<\/strong>/\o033[0m/' \
 	    		-e '/<\/p>/ a\
-			'  \
-			-e '/<br>/ a\
 			' | \
 			sed 's/<[^>]*>//g')
-		printf "$TESTO" >> clog/$FILE_COUNT
+			# Substitutes <h> tags with color green
+			# Substitutes <strong> tags with underline
+			# Adds a newline when paragraph ends
+			# Removes all other tags
+		printf "$TEXT" >> clog/$FILE_COUNT
 	fi
 else
 	printf "\e[32m%s\e[0m\n\n" $TITLE >> clog/$FILE_COUNT
@@ -38,4 +33,4 @@ fi
 printf "\t%s:\t%s\n" $FILE_COUNT "$TITLE" >> clog/index
 vim clog/$FILE_COUNT
 
-printf "Creato post numero %s\n" $FILE_COUNT
+printf "Created post number %s\n" $FILE_COUNT
